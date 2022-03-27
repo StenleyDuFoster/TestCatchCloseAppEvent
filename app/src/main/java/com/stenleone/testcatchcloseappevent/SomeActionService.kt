02bf891848bc76d733caece015d0db3c2ad69200
StanleyDuFoster <stenleone@gmail.com>
2022-03-27 23:31:03 +0300

    ыщьу

--- app/src/main/java/com/stenleone/testcatchcloseappevent/SomeActionService.kt
+++ app/src/main/java/com/stenleone/testcatchcloseappevent/SomeActionService.kt
@@ -1,20 +1,50 @@
 package com.stenleone.testcatchcloseappevent
 
+import android.app.Activity
+import android.app.Application
 import android.app.Service
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Toast
 
 class SomeActionService : Service() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        application.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+
+            }
+
+            override fun onActivityStarted(p0: Activity) {
+
+            }
+
+            override fun onActivityResumed(p0: Activity) {
+
+            }
+
+            override fun onActivityPaused(p0: Activity) {
+
+            }
+
+            override fun onActivityStopped(p0: Activity) {
+
+            }
+
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+
+            }
+
+            override fun onActivityDestroyed(p0: Activity) {
+                Toast.makeText(applicationContext, "Destroyed", Toast.LENGTH_SHORT).show()
+                stopSelf()
+            }
+
+        })
+    }
+
     override fun onBind(p0: Intent?) = null
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return START_STICKY
-    }
-
-    override fun onDestroy() {
-        Toast.makeText(applicationContext, "Destroyed", Toast.LENGTH_SHORT).show()
-        super.onDestroy()
-    }
 
 }
